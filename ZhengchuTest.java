5660208a6c0b4f628beebf1f3b1b939c9eed9fd5
hello20110562 <hello20110562@gmail.com>
2014-12-09 20:31:36 +0800

    124

--- ZhengchuTest.java
+++ ZhengchuTest.java
@@ -1,6 +1,6 @@
 public class ZhengchuTest{
 	public static void main(String args[]){
-	    int j=0,i=0,k;
+	    int j=0,i=0,k;//1、用do while
 		do{
 		i=i+1;
 		k=i%3;
@@ -11,7 +11,7 @@ public class ZhengchuTest{
 		}
 		}
 		while(j<=4);
-		/*int i,j=0,k;//循环语句读数
+		/*int i,j=0,k;//2、用for循环语句读数
 		for (i=1;i<=100;i++){
 		k=i%3;
 		if(k==0)
--- ZhiShuTest.java
+++ ZhiShuTest.java
@@ -12,7 +12,7 @@ public class ZhiShuTest{
 	}*/
 		for(int i=101;i<200;i++)
 		{
-		int sum=0;//sum用于计算整除的个数
+	  //sum用于计算整除的个数
 		for(int j=2;j<i;j++){
 			//i除以j的余数
 			if (i%j==0){   //当j
